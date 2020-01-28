4f4636d1929ce6a6cdb788e33a22fe5b71bc8be1
Peter Sherman <pfnsec@gmail.com>
2020-01-28 17:26:55 +0000

    Initial commit

---
+++ RanDrum.ps1
@@ -0,0 +1,9 @@
+﻿$drumFolders = ""
+$count = 20
+
+
+$drums = Get-ChildItem -Path $drumFolders -Recurse -Include *.mp3, *.wav | Get-Random -Count $count
+
+
+Set-Clipboard
+ForEach-Object -InputObject $drums -Process{Set-Clipboard -Append -LiteralPath $_}
